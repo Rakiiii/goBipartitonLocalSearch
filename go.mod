4cfaf0661e533a44a28415d2b6ac4ece6a7a4241
Smurf <sudaru2@gmail.com>
2019-11-24 12:52:03 +0300

    update graph lib

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/Rakiiii/goBipartitonLocalSearch
 go 1.13
 
 require (
-	github.com/Rakiiii/goGraph v0.2.1
+	github.com/Rakiiii/goGraph v0.2.2
 	github.com/Rakiiii/goPair v0.0.0-20191123194235-c325a5a98042
 	github.com/Rakiiii/goSort v0.0.1
 )
---
+++ specializedgraph_test.go
@@ -0,0 +1,32 @@
+package bipartitonlocalsearchlib
+
+import (
+	"fmt"
+	"testing"
+
+	graphlib "github.com/Rakiiii/goGraph"
+)
+
+func TestParseGraph(t *testing.T) {
+	var parser graphlib.Parser
+	gr, err := parser.ParseUnweightedUndirectedGraphFromFile("test_gr3")
+	if err != nil {
+		t.Error(err)
+	}
+
+	for i := 0; i < gr.AmountOfVertex(); i++ {
+		fmt.Println(gr.GetEdges(i))
+	}
+
+	fmt.Println("am of ver:", gr.AmountOfVertex())
+
+	fmt.Println("newGraph")
+
+	var graph Graph
+	graph.ParseGraph("test_gr3")
+	for i := 0; i < 14; i++ {
+		fmt.Println(graph.GetEdges(i))
+	}
+
+	fmt.Println("am of ver:", graph.AmountOfVertex())
+}
