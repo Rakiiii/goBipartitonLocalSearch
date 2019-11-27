340aeb8784d9d6e155313a4a7a8720b567782f9f
Smurf <sudaru2@gmail.com>
2019-11-27 22:33:08 +0300

    add hugry algorithm of vertex renumbering

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/Rakiiii/goGraph v0.2.2
 	github.com/Rakiiii/goPair v0.0.2
 	github.com/Rakiiii/goSort v0.0.1
+	github.com/Rakiiii/goTuple v0.0.3 // indirect
 )
--- specializedgraph.go
+++ specializedgraph.go
@@ -1,16 +1,20 @@
 package bipartitonlocalsearchlib
 
 import graphlib "github.com/Rakiiii/goGraph"
+import gotuple "github.com/Rakiiii/goTuple"
 
+//Graph if specialization of grpah for biderectional local search
 type Graph struct {
 	graphlib.Graph
 	amountOfIndependent int
 }
 
+//GetAmountOfIndependent returns amount of independent vertex in the graph
 func (g *Graph) GetAmountOfIndependent() int {
 	return g.amountOfIndependent
 }
 
+//NumIndependent renumberring vertex with some independetn subset of vertex
 func (g *Graph) NumIndependent() []int {
 	colorSet := make([]bool, g.AmountOfVertex())
 	amountOfIndependent := 0
@@ -41,9 +45,55 @@ func (g *Graph) NumIndependent() []int {
 	g.amountOfIndependent = amountOfIndependent
 
 	g.RenumVertex(newOrd)
-	return newOrd
+	return newOrd	
 }
 
+//HungryNumIndependent realization of hugnry search algorithm of max independent vertex set with renumbering prev set of vertex 
+func (g *Graph)HungryNumIndependent()[]int{
+	sortedOrd := make([]goTuple.IntTuple,g.AmountOfVertex())
+
+	for i := 0 ; i < g.AmountOfVertex() ; i ++{
+		newOrd[i].First = i
+		newOrd[i].Second = len(g.GetEdges(i))
+		newOrd[i].Third = 0
+	}
+
+	sortedOrd = gotuple.QuicksortIntTupleSecond(sortedOrd)
+
+	optPointers := make([]int,g.AmountOfVertex())
+	for i,num := range sortedOrd{
+		optPointers[num.First] = i
+	}
+
+	newOrd := make([]int,g.AmountOfVertex())
+	counter := 0 
+	 
+	for i, vertex := range sortedOrd{
+		if vertex.Third == 0{
+			newOrd[counter] = vertex.First
+			counter ++
+			for _,vertex := range g.GetEdges(vertex.First){
+				sortedOrd[optPointers[vertex]].Third = 1
+			}
+		}
+	}
+
+	g.AmountOfVertex = counter
+
+	for i,vertex := range sortedOrd{
+		if vertex.Third == 1{
+			newOrd[counter] = vertex.First
+		}
+	}
+
+	g.RenumVertex(newOrd)
+
+	return newOrd
+
+}
+
+
+//ParseGraph parsing graph of metis format from file @path returning errors of reading file
 func (g *Graph) ParseGraph(path string) error {
 	var parser graphlib.Parser
 
